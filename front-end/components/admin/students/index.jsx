954bf5c433e5fa7071239a6c33b3169b61b7db81
Your Nameecho <you@example.com>
2024-07-25 22:30:08 +0530

    student form added copied from register

--- front-end/components/admin/students/index.jsx
+++ front-end/components/admin/students/index.jsx
@@ -1,7 +1,10 @@
 'use client';
 import AdminLayout from "@/components/shared/admin-layout";
 import { BlockOutlined, CalendarOutlined, DeleteOutlined, EditOutlined, EnvironmentOutlined, MailOutlined, MoneyCollectOutlined, MoreOutlined, SearchOutlined, UserOutlined, VideoCameraOutlined } from "@ant-design/icons";
-import { Avatar, Button, Card, Divider, Dropdown, Input, Pagination, Tooltip } from "antd";
+import { Avatar, Button, Card, Divider, Drawer, Dropdown, Form, Input, Pagination, Select, Tag, Tooltip } from "antd";
+import { useState } from "react";
+
+const { Item } = Form;
 
 // options to format date
 const options = {
@@ -64,6 +67,51 @@ const students = [
 
 const Students = () => {
 
+    // States collection
+    const [open, setOpen] = useState(false);
+
+    // for multiple select
+    const options = [
+        {
+            value: 'gold',
+        },
+        {
+            value: 'lime',
+        },
+        {
+            value: 'green',
+        },
+        {
+            value: 'cyan',
+        },
+    ];
+
+    const tagRender = (props) => {
+        const { label, value, closable, onClose } = props;
+        const onPreventMouseDown = (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        };
+        return (
+            <Tag
+                color={value}
+                onMouseDown={onPreventMouseDown}
+                closable={closable}
+                onClose={onClose}
+                style={{
+                    marginInlineEnd: 4,
+                }}
+            >
+                {label}
+            </Tag>
+        );
+    };
+
+    // get values when form submitted
+    const onFinish = (values) => {
+        console.log(values);
+    }
+
     // Adding toolbar for students page
     const Toolbar = () => {
         return (
@@ -87,6 +135,7 @@ const Students = () => {
                         size="large"
                         style={{ borderRadius: 0 }}
                         icon={<UserOutlined />}
+                        onClick={() => setOpen(true)}
                     >Enroll</Button>
                 </>
             </>
@@ -244,6 +293,176 @@ const Students = () => {
                     <Pagination total={500} itemRender={itemRender} />
                 </div>
             </div>
+
+            <Drawer
+                title="Enroll a Student"
+                open={open}
+                onClose={() => setOpen(false)}
+                width={720}
+            >
+                <Form onFinish={onFinish} layout="vertical">
+                    <div className="grid md:grid-cols-2 gap-3">
+                        <Item
+                            name={'fullname'}
+                            label="FullName"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter your fullname"
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="FullName"
+                                style={{ borderRadius: 0 }}
+                                size="large"
+                            />
+                        </Item>
+                        <Item
+                            name={'mobile'}
+                            label="Mobile"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter your mobile"
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="+91 9545282408"
+                                style={{ borderRadius: 0 }}
+                                size="large"
+                                type="tel"
+                            />
+                        </Item>
+                    </div>
+                    <div className="grid md:grid-cols-2 gap-3">
+                        <Item
+                            name={'email'}
+                            label="Email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter your email address"
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="Email"
+                                style={{ borderRadius: 0 }}
+                                size="large"
+                            />
+                        </Item>
+                        <Item
+                            name={'password'}
+                            label="Password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter your password"
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="********"
+                                style={{ borderRadius: 0 }}
+                                size="large"
+                                type="password"
+                            />
+                        </Item>
+                    </div>
+                    <div className="grid md:grid-cols-2 gap-3">
+                        <Item
+                            name={'father'}
+                            label="Father Name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter your father name"
+                                }
+                            ]}
+                        >
+                            <Input
+                                placeholder="Father Name"
+                                style={{ borderRadius: 0 }}
+                                size="large"
+                            />
+                        </Item>
+                        <Item
+                            name={'dob'}
+                            label="DOB"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter your DOB"
+                                }
+                            ]}
+                        >
+                            <Input
+                                style={{ borderRadius: 0 }}
+                                size="large"
+                                type="date"
+                            />
+                        </Item>
+                    </div>
+                    <div className="grid md:grid-cols-2 gap-3">
+                        <Item
+                            name={'qualification'}
+                            label="Qualification"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please select your qualification"
+                                }
+                            ]}
+                        >
+                            <Select
+                                showSearch
+                                placeholder="Select Qualification"
+                                filterOption={(input, option) =>
+                                    (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                                }
+                                options={[
+                                    { value: '10th', label: 'High School' },
+                                    { value: '12th', label: 'Intermediate' },
+                                    { value: 'ug', label: 'UG' },
+                                    { value: 'pg', label: 'PG' },
+                                ]}
+                            />
+                        </Item>
+                        <Item
+                            name={'courses'}
+                            label="Courses"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please choose your Courses"
+                                }
+                            ]}
+                        >
+                            <Select
+                                placeholder="Choose your Courses"
+                                mode="multiple"
+                                tagRender={tagRender}
+                                style={{
+                                    width: '100%',
+                                }}
+                                options={options}
+                            />
+                        </Item>
+                    </div>
+                    <Item>
+                        <Button
+                            className="w-full bg-indigo-600 text-white font-semibold"
+                            size="large"
+                            style={{ borderRadius: 0 }}
+                            htmlType="submit"
+                        >
+                            Register
+                        </Button>
+                    </Item>
+                </Form>
+            </Drawer>
         </AdminLayout>
     )
 }
