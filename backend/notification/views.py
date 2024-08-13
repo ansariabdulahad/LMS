44ccf8b8c923785f6597b5220d728e40d8510d64
Your Nameecho <you@example.com>
2024-08-13 20:58:42 +0530

    sample api of crud created for notification

--- backend/notification/views.py
+++ backend/notification/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+# Fetch function for fetching the data
+def fetch(req) :
+    return Response({
+        'message': 'Data has been fetched'
+    })
+
+# create function to add the data
+def create(req) :
+    return Response({
+        'message': 'Data has been created'
+    })
+
+# update function to update the data
+def update(req) :
+    return Response({
+        'message': 'Data has been updated'
+    })
+
+# trash function to trash the data
+def trash(req) :
+    return Response({
+        'message': 'Data has been deleted'
+    })
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def main(req) :
+    if req.method == 'GET' : return fetch(req)
+    elif req.method == 'POST' : return create(req)
+    elif req.method == 'PUT' : return update(req)
+    else : return trash(req)
