411d922ffc23ca4d9b947ae1cf610047521a16f7
Your Nameecho <you@example.com>
2024-08-22 14:40:55 +0530

    removed warning by adding # type: ignore to the package while importing

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView # type: ignore
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer # type: ignore
 from .serializers import AuSerializer
 
 class TokenSerializer(TokenObtainPairSerializer) :
